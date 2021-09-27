f3bff2f4fa8eaea50c2dbb8ef2e6389e803816ee
Juanse Sanchez <sanchez.iag2@gmail.com>
2021-09-27 13:39:59 -0600

    removed unnecessary usings and comments

--- Lab1 Compiladores/Program.cs
+++ Lab1 Compiladores/Program.cs
@@ -6,15 +6,6 @@ namespace Lab1_Compiladores
     {
         static void Main(string[] args)
         {
-            //Scanner scanner = new Scanner("123-45+-*/");
-            //Token nexttoken;
-            //do
-            //{
-            //    nexttoken = scanner.GetToken();
-            //    Console.WriteLine("token: {0}, valor: {1}", nexttoken.Tag, nexttoken.Value);
-            //} while (nexttoken.Tag != TokenType.EOF);
-
-            //-2+8*4/-(5-3)
             //string regexp = Console.ReadLine();
             Parser prser = new Parser();
             Console.WriteLine(prser.Parse("-2+8*4/(5-3)"));
